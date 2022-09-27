5ad68b61371a7d8509d2280bab1486096cfa66c7
islamshu12 <islamshu12@gmail.com>
2022-09-27 21:25:20 +0300

    edit

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -60,12 +60,12 @@
 
     </div>
 
-
+{{-- 
 
     <img src="{{asset('front/images/circle.png')}}" class="img-back anmation1" alt="" />
     <img src="{{asset('front/images/Rectangle 3 copy@2x.png')}}" class="img-back-3 anmation3" alt="" />
     <img src="{{asset('front/images/Rectangle 3@2x.png')}}" class="img-back-2 anmation2" alt="" />
-    <img src="{{asset('front/images/Triangle 1@2x.png')}}" class="img-back-1 anmation4" alt="" />
+    <img src="{{asset('front/images/Triangle 1@2x.png')}}" class="img-back-1 anmation4" alt="" /> --}}
 
 
   </div>
