506304caa5c289a3465d021ec2e5f557c49d77f2
islamshu12 <islamshu12@gmail.com>
2022-10-03 17:42:33 +0300

    edit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -460,7 +460,7 @@
                         <h3>{!! Illuminate\Support\Str::limit($item->title, 40) !!} </h3>
                         <p>
                             {{-- {!! Illuminate\Support\Str::limit($item->description, 80) !!} --}}
-                            {!! Str::words($item->description, 70, ' ...') !!}
+                            {!! Str::words($item->description, 20, ' ...') !!}
                         </p>
 
                         <span>{{ \Carbon\Carbon::parse($item->created_at)->format('Y/m/d') }}</span>
